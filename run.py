9edf9beee73c6368f3923c30210b8782d676f5f3
jimmybow <jimmybow@hotmail.com.tw>
2017-07-06 13:54:22 +0800

    Update run.py

--- run.py
+++ run.py
@@ -23,5 +23,14 @@ app.layout = html.Div(children=[
                 'title': 'Dash Data Visualization'
             }
         }
-    )
+    ),
+    dcc.Input(id='my-id', value='initial value', type="text"),
+    html.Div(id='my-div')
 ])
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
